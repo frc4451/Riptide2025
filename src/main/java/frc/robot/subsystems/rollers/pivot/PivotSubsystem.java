a63963bae62b983bab7bfafc960c5eeb4d63687c
judsonjames <judsonjames96@gmail.com>
2025-01-10 17:42:58 -0500

    Include example of LoggedMechanism2d

--- src/main/java/frc/robot/subsystems/rollers/pivot/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/rollers/pivot/PivotSubsystem.java
@@ -1,9 +1,19 @@
 package frc.robot.subsystems.rollers.pivot;
 
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.subsystems.rollers.single.SingleRollerSubsystem;
 
 public class PivotSubsystem extends SingleRollerSubsystem {
+  private final PivotVisualizer pivotVisualizer = new PivotVisualizer("Measured", Color.kBlack);
+
   public PivotSubsystem(PivotIO io) {
     super("Pivot", io);
+  }
+
+  @Override
+  public void periodic() {
+    super.periodic();
+
+    this.pivotVisualizer.update(this.inputs.positionRad);
   }
 }
