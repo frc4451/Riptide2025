15881b1c6b0da8018e316a1cf643bee83a8fd2a2
Willem Nordhus <w.a.nordhus@gmail.com>
2025-01-18 16:51:45 -0500

    start Chute Subsystem

---
+++ src/main/java/frc/robot/subsystems/superstructure/coral/Chute.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.superstructure.coral;
+
+import frc.robot.subsystems.rollers.single.SingleRollerIO;
+import frc.robot.subsystems.rollers.single.SingleRollerSubsystem;
+
+public class Chute extends SingleRollerSubsystem {
+  public Chute(SingleRollerIO io) {
+    super("Chute", io);
+  }
+}
