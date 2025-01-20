d79d4c9a499fff824acf9b62edde217bff0e151e
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2025-01-20 17:11:06 -0500

    Format

--- src/main/java/frc/robot/subsystems/quest/QuestConstants.java
+++ src/main/java/frc/robot/subsystems/quest/QuestConstants.java
@@ -6,16 +6,11 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public class QuestConstants {
-    public static final Transform3d robotToQuest = new Transform3d(
-        new Translation3d(
-            Units.inchesToMeters(0),
-            Units.inchesToMeters(13.187500),
-            Units.inchesToMeters(5.415796)
-        ),
-        new Rotation3d(
-            0.0,
-            0.0,
-            0.0
-        )
-    ) ;
+  public static final Transform3d robotToQuest =
+      new Transform3d(
+          new Translation3d(
+              Units.inchesToMeters(0),
+              Units.inchesToMeters(13.187500),
+              Units.inchesToMeters(5.415796)),
+          new Rotation3d(0.0, 0.0, 0.0));
 }
