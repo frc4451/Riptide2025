1e9cb695376cd9dc1277a0d271c07bd7da5866c7
4451 driver station <4451driverstation@gmail.com>
2025-01-20 18:59:55 -0500

    Made chute and shooter subsystems

--- src/main/java/frc/robot/subsystems/superstructure/coral/Chute.java
+++ src/main/java/frc/robot/subsystems/superstructure/coral/Chute.java
@@ -1,10 +1,19 @@
 package frc.robot.subsystems.superstructure.coral;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.rollers.single.SingleRollerIO;
 import frc.robot.subsystems.rollers.single.SingleRollerSubsystem;
 
 public class Chute extends SingleRollerSubsystem {
   public Chute(SingleRollerIO io) {
     super("Chute", io);
+  }
+
+  public void runVelocity(double velocity) {
+    io.runVelocity(velocity);
+  }
+
+  public Command runVelocityCommand(double velocity) {
+    return run(() -> runVelocity(velocity));
   }
 }
