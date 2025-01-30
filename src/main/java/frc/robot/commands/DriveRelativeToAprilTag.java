29b354639da26777e257a3ad165908001f435ab6
prophetospam <w.a.nordhus@gmail.com>
2025-01-29 23:35:02 -0500

    more work on perpindicular drive

--- src/main/java/frc/robot/commands/DriveRelativeToAprilTag.java
+++ src/main/java/frc/robot/commands/DriveRelativeToAprilTag.java
@@ -1,13 +1,11 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,7 +14,6 @@ import frc.robot.bobot_state.BobotState;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.vision.VisionConstants;
 import java.util.function.Supplier;
-import org.littletonrobotics.junction.Logger;
 
 public class DriveRelativeToAprilTag {
   public Command driveToOffsetCommand(
@@ -74,31 +71,25 @@ public class DriveRelativeToAprilTag {
     return Commands.run(
             () -> {
               Pose2d robotPose = BobotState.getGlobalPose();
-                Pose2d targetPose = maybeTargetPose.get();
+              Pose2d targetPose = maybeTargetPose.get();
 
               Rotation2d desiredTheta = targetPose.getRotation().unaryMinus();
 
-              
-            //   https://en.wikipedia.org/wiki/Vector_projection#Scalar_projection
-              Translation2d robotToTargetGlobalSpace = robotPose.minus(targetPose).getTranslation();
-              Rotation2d globalToRelativeAngle = targetPose.getRotation().minus(robotToTargetGlobalSpace.getAngle());
-              double parallelError = robotToTargetGlobalSpace.getNorm() * globalToRelativeAngle.getSin();
-              Logger.recordOutput("test", parallelError);
+              //   https://en.wikipedia.org/wiki/Vector_projection#Scalar_projection
+              Translation2d robotToTarget = robotPose.minus(targetPose).getTranslation();
+              Rotation2d angleBetween = robotToTarget.getAngle();
+              double parallelError = -robotToTarget.getNorm() * angleBetween.getSin();
 
               Rotation2d thetaError = robotPose.getRotation().minus(desiredTheta);
 
               double parallelSpeed = parallelController.calculate(parallelError);
 
-              double xSpeed = parallelSpeed * -targetPose.getRotation().getSin() + -perpendicularInput.get() * targetPose.getRotation().getCos();
-              double ySpeed = parallelSpeed * targetPose.getRotation().getCos() + -perpendicularInput.get() * targetPose.getRotation().getSin();
-
               double angularSpeed = thetaController.calculate(thetaError.getRadians());
 
+              //   The error is here, need to fix
               ChassisSpeeds speeds =
-                  new ChassisSpeeds(
-                      xSpeed * drive.getMaxLinearSpeedMetersPerSec(),
-                      ySpeed * drive.getMaxLinearSpeedMetersPerSec(),
-                      angularSpeed);
+                  ChassisSpeeds.fromRobotRelativeSpeeds(
+                      perpendicularInput.get(), parallelSpeed, angularSpeed, desiredTheta);
               boolean isFlipped =
                   DriverStation.getAlliance().isPresent()
                       && DriverStation.getAlliance().get() == Alliance.Red;
