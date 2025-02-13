0c58e3f3f4693d99fd16495bb062f4f0427ab9cc
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2025-02-13 18:02:41 -0500

    Remove unused `AlgaeShooterConstants`

--- src/main/java/frc/robot/subsystems/superstructure/constants/AlgaeShooterConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.superstructure.constants;
-
-public class AlgaeShooterConstants {
-
-  public static final int canId = 0;
-  public static final double reduction = 0;
-  public static final double currentLimitAmps = 0;
-  public static final boolean invert = false;
-}
