193adfa321eecc813a40bc66453b30c0b196a392
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2025-03-21 08:18:53 -0400

    Update at 'Fri Mar 21 08:18:52 EDT 2025'

--- src/main/java/frc/robot/subsystems/superstructure/modes/SuperStructureModes.java
+++ src/main/java/frc/robot/subsystems/superstructure/modes/SuperStructureModes.java
@@ -10,8 +10,8 @@ public enum SuperStructureModes {
   L3Coral(36.0 / 2.0, SuperStructureModes.TUCKED.coralPos),
   L4Coral(50.0 / 2.0, SuperStructureModes.TUCKED_L4.coralPos),
   FLOOR_ALGAE(1.0 / 2.0, Rotation2d.fromDegrees(-40.0)),
-  L2Algae(20.0 / 2.0, Rotation2d.fromDegrees(-10.0)),
-  L3Algae(37.0 / 2.0, Rotation2d.fromDegrees(-10.0)),
+  L2Algae(23.0 / 2.0, Rotation2d.fromDegrees(-10.0)),
+  L3Algae(40.0 / 2.0, Rotation2d.fromDegrees(-10.0)),
   Barge(50.0 / 2.0, Rotation2d.fromDegrees(90));
 
   public final double elevatorHeightInches;
