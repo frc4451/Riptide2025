8623b8d73b963bd998759f264fcef4364805b44b
Judson James <judsonjames96@gmail.com>
2025-03-29 13:49:00 -0400

    remove extraneous ChoreoPaths entries

--- src/main/java/frc/robot/auto/ChoreoPaths.java
+++ src/main/java/frc/robot/auto/ChoreoPaths.java
@@ -13,17 +13,8 @@ public enum ChoreoPaths {
   START_TOP_TO_IL4("StartTop-IL4"),
 
   // Start Mid to ___
-  START_MID_TO_AL4("StartMid-AL4"),
-  START_MID_TO_CL4("StartMid-CL4"),
-  START_MID_TO_DL4("StartMid-DL4"),
-  START_MID_TO_EL4("StartMid-EL4"),
-  START_MID_TO_FL4("StartMid-FL4"),
   START_MID_TO_GL4("StartMid-GL4"),
   START_MID_TO_HL4("StartMid-HL4"),
-  START_MID_TO_IL4("StartMid-IL4"),
-  START_MID_TO_JL4("StartMid-JL4"),
-  START_MID_TO_KL4("StartMid-KL4"),
-  START_MID_TO_LL4("StartMid-LL4"),
 
   // Start Bottom to ___
   START_BOTTOM_TO_EL2("StartBottom-EL2"),
@@ -32,7 +23,6 @@ public enum ChoreoPaths {
   START_BOTTOM_TO_FL4("StartBottom-FL4"),
 
   // ___ to HPS Right
-  AL4_TO_HPS_RIGHT("AL4-HPSRight"),
   BL4_TO_HPS_RIGHT("BL4-HPSRight"),
   CL4_TO_HPS_RIGHT("CL4-HPSRight"),
   DL4_TO_HPS_RIGHT("DL4-HPSRight"),
@@ -41,14 +31,8 @@ public enum ChoreoPaths {
   FL2_TO_HPS_RIGHT("FL2-HPSRight"),
   FL4_TO_HPS_RIGHT("FL4-HPSRight"),
   GL4_TO_HPS_RIGHT("GL4-HPSRight"),
-  HL4_TO_HPS_RIGHT("HL4-HPSRight"),
-  IL4_TO_HPS_RIGHT("IL4-HPSRight"),
-  JL4_TO_HPS_RIGHT("JL4-HPSRight"),
-  KL4_TO_HPS_RIGHT("KL4-HPSRight"),
-  LL4_TO_HPS_RIGHT("LL4-HPSRight"),
 
   // HPS Right to ___
-  HPS_RIGHT_TO_AL4("HPSRight-AL4"),
   HPS_RIGHT_TO_BL4("HPSRight-BL4"),
   HPS_RIGHT_TO_CL4("HPSRight-CL4"),
   HPS_RIGHT_TO_DL4("HPSRight-DL4"),
@@ -56,20 +40,9 @@ public enum ChoreoPaths {
   HPS_RIGHT_TO_EL4("HPSRight-EL4"),
   HPS_RIGHT_TO_FL4("HPSRight-FL4"),
   HPS_RIGHT_TO_GL4("HPSRight-GL4"),
-  HPS_RIGHT_TO_HL4("HPSRight-HL4"),
-  HPS_RIGHT_TO_IL4("HPSRight-IL4"),
-  HPS_RIGHT_TO_JL4("HPSRight-JL4"),
-  HPS_RIGHT_TO_KL4("HPSRight-KL4"),
-  HPS_RIGHT_TO_LL4("HPSRight-LL4"),
 
   // ___ to HPS Left
   AL4_TO_HPS_LEFT("AL4-HPSLeft"),
-  BL4_TO_HPS_LEFT("BL4-HPSLeft"),
-  CL4_TO_HPS_LEFT("CL4-HPSLeft"),
-  DL4_TO_HPS_LEFT("DL4-HPSLeft"),
-  EL4_TO_HPS_LEFT("EL4-HPSLeft"),
-  FL4_TO_HPS_LEFT("FL4-HPSLeft"),
-  GL4_TO_HPS_LEFT("GL4-HPSLeft"),
   HL4_TO_HPS_LEFT("HL4-HPSLeft"),
   IL4_TO_HPS_LEFT("IL4-HPSLeft"),
   JL4_TO_HPS_LEFT("JL4-HPSLeft"),
