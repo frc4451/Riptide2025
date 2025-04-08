b3c4feec1999756abd5e50a8d0f511609fb7af2d
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2025-04-08 19:02:00 -0400

    Shooter 4V

--- src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterModes.java
+++ src/main/java/frc/robot/subsystems/superstructure/shooter/ShooterModes.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems.superstructure.shooter;
 public enum ShooterModes {
   NONE(0.0),
   INTAKE(3.0),
-  SHOOT(3.5),
-  L4_SHOOT(-3.5),
+  SHOOT(4.0),
+  L4_SHOOT(-4.0),
   ALGAE_INTAKING(-9.0),
   ALGAE_INTAKED(-1.5),
   ALGAE_SHOOT(12.0),
