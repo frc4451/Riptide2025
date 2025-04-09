85f8df2c3ef36e6e7f242fd723c4ebcd07a77b30
Lewis Arias <lewisgarias@outlook.com>
2025-04-08 22:33:56 -0400

    Improved version of `DriveToPose` that controls the translational speed rather than x & y velocity. Heavily references 6328's `AutoAlignController` from 2024
    Does the following differently:
    1) This is field relative now (unlike previous impl) so if our rotation
    isn't perfect we shouldn't stray to the side; for example if our
    rotation is 180 degrees off we won't drive away from our target and off
    the field
    2) It controls translational magnitude (magnitude of x & y) rather than
    controlling them as two separate values, I think that this'll be the key
    to not slamming from far distances
    
    https://github.com/Mechanical-Advantage/RobotCode2024/blob/main/src/main/java/org/littletonrobotics/frc2024/subsystems/drive/controllers/AutoAlignController.java#L135 was the main reference for this

--- src/main/java/frc/robot/commands/DriveToPoseCommand.java
+++ src/main/java/frc/robot/commands/DriveToPoseCommand.java
@@ -2,7 +2,8 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -12,9 +13,7 @@ import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
 public class DriveToPoseCommand extends Command {
-  private final ProfiledPIDController perpendicularController =
-      DriveCommandConstants.makeTranslationController();
-  private final ProfiledPIDController parallelController =
+  private final ProfiledPIDController distanceController =
       DriveCommandConstants.makeTranslationController();
   private final ProfiledPIDController angleController = DriveCommandConstants.makeAngleController();
 
@@ -37,49 +36,47 @@ public class DriveToPoseCommand extends Command {
     return this;
   }
 
+  // Heavily inspired by 6328's `AutoAlignController` from 2024
+  // https://github.com/Mechanical-Advantage/RobotCode2024/blob/main/src/main/java/org/littletonrobotics/frc2024/subsystems/drive/controllers/AutoAlignController.java#L135
   @Override
   public void execute() {
-    Pose2d robotPose = useConstrainedPose ? drive.getConstrainedPose() : drive.getGlobalPose();
-    Pose2d targetPose = targetPoseSupplier.get();
-    Logger.recordOutput("Commands/" + getName() + "/TargetPose", targetPose);
+    Pose2d robot = useConstrainedPose ? drive.getConstrainedPose() : drive.getGlobalPose();
+    Pose2d target = targetPoseSupplier.get();
 
-    Transform2d error = robotPose.minus(targetPose);
-    Logger.recordOutput("Commands/" + getName() + "/Error", error);
+    Translation2d translationalError = robot.getTranslation().minus(target.getTranslation());
+    Rotation2d angularError = robot.getRotation().minus(target.getRotation());
 
-    double perpendicularSpeed = perpendicularController.calculate(error.getX(), 0);
-    perpendicularSpeed = !perpendicularController.atSetpoint() ? perpendicularSpeed : 0;
+    // Magnitude of translational speed, meaning that x & y are controlled together
+    double translationalSpeed = distanceController.calculate(translationalError.getNorm(), 0);
+    translationalSpeed = !distanceController.atGoal() ? translationalSpeed : 0;
 
-    double parallelSpeed = parallelController.calculate(error.getY(), 0);
-    parallelSpeed = !parallelController.atSetpoint() ? parallelSpeed : 0;
+    double anglularVelocity = angleController.calculate(angularError.getRadians(), 0);
+    anglularVelocity = !angleController.atGoal() ? anglularVelocity : 0;
 
-    double angularSpeed = angleController.calculate(error.getRotation().getRadians(), 0);
-    angularSpeed = !angleController.atSetpoint() ? angularSpeed : 0;
+    Translation2d velocity = new Translation2d(translationalSpeed, translationalError.getAngle());
 
-    ChassisSpeeds speeds = new ChassisSpeeds(perpendicularSpeed, parallelSpeed, angularSpeed);
+    ChassisSpeeds speeds =
+        ChassisSpeeds.fromFieldRelativeSpeeds(
+            velocity.getX(), velocity.getY(), anglularVelocity, robot.getRotation());
 
     drive.runVelocity(speeds);
 
-    Logger.recordOutput("Commands/" + getName() + "/IsAtSetpoint", atSetpoint().getAsBoolean());
-    Logger.recordOutput(
-        "Commands/" + getName() + "/PerpendincularAtSetpoint",
-        perpendicularController.atSetpoint());
-    Logger.recordOutput(
-        "Commands/" + getName() + "/ParallelAtSetpoint", parallelController.atSetpoint());
-    Logger.recordOutput("Commands/" + getName() + "/AngleAtSetpoint", angleController.atSetpoint());
+    Logger.recordOutput("Commands/" + getName() + "/Robot", robot);
+    Logger.recordOutput("Commands/" + getName() + "/Target", target);
+    Logger.recordOutput("Commands/" + getName() + "/Error/Translational", translationalError);
+    Logger.recordOutput("Commands/" + getName() + "/Error/Angular", angularError);
+    Logger.recordOutput("Commands/" + getName() + "/Speeds/TranslationalSpeed", translationalSpeed);
+    Logger.recordOutput("Commands/" + getName() + "/Speeds/AngularVelocity", anglularVelocity);
+    Logger.recordOutput("Commands/" + getName() + "/Speeds/ChassisSpeeds", speeds);
   }
 
   @Override
   public void end(boolean interrupt) {
-    perpendicularController.reset(0);
-    parallelController.reset(0);
+    distanceController.reset(0);
     angleController.reset(0);
   }
 
   public Trigger atSetpoint() {
-    return new Trigger(
-        () ->
-            perpendicularController.atSetpoint()
-                && parallelController.atSetpoint()
-                && angleController.atSetpoint());
+    return new Trigger(() -> distanceController.atSetpoint() && angleController.atSetpoint());
   }
 }
