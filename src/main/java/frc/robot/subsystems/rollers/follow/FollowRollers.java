8f7bcbbce40f81c85c674e5f3705c3643126796a
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2025-04-18 08:36:08 -0500

    Log leader and follower connected state separately

--- src/main/java/frc/robot/subsystems/rollers/follow/FollowRollers.java
+++ src/main/java/frc/robot/subsystems/rollers/follow/FollowRollers.java
@@ -11,19 +11,23 @@ public class FollowRollers {
   protected final FollowRollersMagicIOInputsAutoLogged inputs =
       new FollowRollersMagicIOInputsAutoLogged();
 
-  private final Alert disconnected;
+  private final Alert leaderDisconnected;
+  private final Alert followerDisconnected;
 
   public FollowRollers(String name, FollowRollersIO io) {
     this.name = name;
     this.io = io;
 
-    disconnected = new Alert(name + " motor disconnected!", Alert.AlertType.kWarning);
+    leaderDisconnected = new Alert(name + " leader motor disconnected!", Alert.AlertType.kWarning);
+    followerDisconnected =
+        new Alert(name + " follower motor disconnected!", Alert.AlertType.kWarning);
   }
 
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs(name, inputs);
-    disconnected.set(!inputs.connected);
+    leaderDisconnected.set(!inputs.leaderConnected);
+    followerDisconnected.set(!inputs.followerConnected);
 
     if (DriverStation.isDisabled()) {
       io.stop();
